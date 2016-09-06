ca48d1f674942779f4161e0c33e12e4bc5597149
Jessica <jessica_216395@hotmail.com>
2016-09-06 19:38:36 -0400

    Created ch. 2 exercises

---
+++ ch2-exercises.py
@@ -0,0 +1,12 @@
+#
+#
+# name          : Jessica Chen
+# email         : jzc22@pitt.edu
+# date          : 09/06/2016
+# class         : CS0008-f2016
+# instructor    : Max Novelli
+#
+# Description:
+# Starting with Python, Chapter 2 Exercises
+#
+# Exercise 3
