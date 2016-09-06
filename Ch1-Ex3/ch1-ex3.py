6f1226bf64ea204c7c564bdeb7b6e95096d8115b
Jessica <jessica_216395@hotmail.com>
2016-09-06 16:23:14 -0400

    Executed Exercise 3 from Chapter 1

---
+++ Ch1-Ex3/ch1-ex3.py
@@ -0,0 +1,11 @@
+#
+#
+# name          : Jessica Chen
+# email         : jzc22@pitt.edu
+# date          : 09/06/2016
+# class         : CS0008-f2016
+# instructor    : Max Novelli
+#
+# Description:
+# Starting with Python, Chapter 1, Exercise 3
+
