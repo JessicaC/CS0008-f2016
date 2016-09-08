6b5c798199c99f3e93fbcc488449b7dc9318337f
Jessica <jessica_216395@hotmail.com>
2016-09-08 14:51:37 -0400

    Attempting Ex. 3

--- ch2-exercises.py
+++ ch2-exercises.py
@@ -10,3 +10,8 @@
 # Starting with Python, Chapter 2 Exercises
 #
 # Exercise 3
+sqm = int(input("Enter the total square meters of land: "))
+acres = sqm / 4,046.8564224
+print ("This is equivalent to " acres "acres.")
+
+# Exercise 5
