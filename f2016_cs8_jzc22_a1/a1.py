9cdb1ebc78c80115ddfb19f5334e0bf0d7e4b380
Jessica <jessica_216395@hotmail.com>
2016-09-27 16:14:32 -0400

    Removed notes

--- f2016_cs8_jzc22_a1/a1.py
+++ f2016_cs8_jzc22_a1/a1.py
@@ -11,8 +11,6 @@
 #
 # Prompts the user which system he/she wants to use
 #
-# Notes to TA/professor:
-# I hope I won't lose points for my folder name not matching the instructions exactly?
 #
 sys = input("Which system do you want to use, USC or Metric?")
 # Ask user for distance driven and how much gasoline was used; the units depend on the system selected by user
