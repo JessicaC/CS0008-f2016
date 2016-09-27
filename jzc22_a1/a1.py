ba951707eefc6e967808730398f232464414cde4
Jessica <jessica_216395@hotmail.com>
2016-09-27 14:46:07 -0400

    comments added

---
+++ jzc22_a1/a1.py
@@ -0,0 +1,12 @@
+#
+#
+# name          : Jessica Chen
+# email         : jzc22@pitt.edu
+# date          : 09/27/2016
+# class         : CS0008-f2016
+# instructor    : Max Novelli
+#
+# Description:
+# Assignment 1
+#
+#
