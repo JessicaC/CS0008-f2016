7293007d99dc7041643762f43735f7eb6c09b681
Jessica <jessica_216395@hotmail.com>
2016-09-28 14:32:40 -0400

    Changes and more changes

--- f2016_cs8_jzc22_a1/f2016_cs8_jzc22_a1.py
+++ f2016_cs8_jzc22_a1/f2016_cs8_jzc22_a1.py
@@ -17,20 +17,37 @@ if sys == 'USC':
     gallons = float(input("Gallons of gasoline used: "))
     km = miles * 1.60934
     liters = gallons * 3.78541
-    print(format(miles, '.3f') + ' miles')
-    print(format(gallons, '.3f') + ' gallons')
-    print(format(km, ',.3f') + ' km')
-    print(format(liters, '.3f') + ' liters')
+    print(format(miles, '6.3f') + ' miles')
+    print(format(gallons, '6.3f') + ' gallons')
+    print(format(km, '6.3f') + ' km')
+    print(format(liters, '6.3f') + ' liters')
 elif sys == 'Metric':
     km = float(input("Number of km driven: "))
     liters = float(input("Liters of gasoline used: "))
     miles = km / 0.621371
     gallons = liters / 0.264172
-    print(format(miles, '.3f') + ' miles')
-    print(format(gallons, '.3f') + ' gallons')
-    print(format(km, '.3f') + ' km')
-    print(format(liters, '.3f') + ' liters')
+    print(format(miles, '.63f') + ' miles')
+    print(format(gallons, '6.3f') + ' gallons')
+    print(format(km, '6.3f') + ' km')
+    print(format(liters, '6.3f') + ' liters')
 mpg = miles / gallons
-print(format(mpg, ',.3f') + ' mpg')
+print(format(mpg, '.3f') + ' mpg')
+# Consumption rating
 cm = 100 * liters / km
+cm1 = 20
+cm2 = 15
+cm3 = 10
+cm4 = 8
+if cm > cm1:
+    rating = 'Extremely Poor'
+elif cm >= cm2:
+    rating = 'Poor'
+elif cm >= cm3:
+    rating = 'Average'
+elif cm >= cm4:
+    rating = 'Good'
+elif cm <= cm4:
+    rating = 'Excellent'
 print (format(cm, '.3f') + ' 1/100km')
+print('Gas consumption rating: ' + rating)
+
