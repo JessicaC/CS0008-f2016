cf6d0631fead96a9f7c8c32a649f26e53d93b9c3
Jessica <jessica_216395@hotmail.com>
2016-09-28 14:10:49 -0400

    some changes

--- f2016_cs8_jzc22_a1/f2016_cs8_jzc22_a1.py
+++ f2016_cs8_jzc22_a1/f2016_cs8_jzc22_a1.py
@@ -12,37 +12,25 @@
 # Prompts the user which system he/she wants to use
 sys = input("Which system do you want to use, USC or Metric? ")
 # Ask user for distance driven and how much gasoline was used; the units depend on the system selected by user
-distance = float(input("Distance driven: "))
-gasoline = float(input("Gasoline used: "))
 if sys == 'USC':
-    distance_units = 'miles'
-    gasoline_units = 'gallons'
-    print(format(distance, '6.3f') + distance_units)
-    print(format(gasoline, '6.3f') + gasoline_units)
+    miles = float(input("Number of miles driven: "))
+    gallons = float(input("Gallons of gasoline used: "))
+    km = miles * 1.60934
+    liters = gallons * 3.78541
+    print(format(miles, '.3f') + ' miles')
+    print(format(gallons, '.3f') + ' gallons')
+    print(format(km, ',.3f') + ' km')
+    print(format(liters, '.3f') + ' liters')
 elif sys == 'Metric':
-    distance_units = 'km'
-    gasoline_units = 'liters'
-    print(format(distance, '6.3f') + distance_units)
-    print(format(gasoline, '6.3f') + gasoline_units)
-# Consumption rating
-cm = 100 * gasoline / distance
-cm1 = 20
-cm2 = 15
-cm3 = 10
-cm4 = 8
-if cm > cm1:
-    rating = 'Extremely Poor'
-elif cm >= cm2:
-    rating = 'Poor'
-elif cm >= cm3:
-    rating = 'Average'
-elif cm >= cm4:
-    rating = 'Good'
-elif cm <= cm4:
-    rating = 'Excellent'
-
-print('\t\t\t\t\t\tUSC\t\tMetric')
-print('Distance\t\t: ')
-print('Gas\t\t\t\t: ')
-print('Consumption\t\t: ')
-print('Gas consumption rating: ' + rating, sep='')
+    km = float(input("Number of km driven: "))
+    liters = float(input("Liters of gasoline used: "))
+    miles = km / 0.621371
+    gallons = liters / 0.264172
+    print(format(miles, '.3f') + ' miles')
+    print(format(gallons, '.3f') + ' gallons')
+    print(format(km, '.3f') + ' km')
+    print(format(liters, '.3f') + ' liters')
+mpg = miles / gallons
+print(format(mpg, ',.3f') + ' mpg')
+cm = 100 * liters / km
+print (format(cm, '.3f') + ' 1/100km')
