75181b64d2e26052c3c3d85c28c1ae30efeacf0b
Jessica <jessica_216395@hotmail.com>
2016-10-25 17:19:39 -0400

    Added code

---
+++ f2016_cs8_jzc22_a2/f2016_cs8_jzc22_a2.py
@@ -0,0 +1,19 @@
+#
+#
+# name          : Jessica Chen
+# email         : jzc22@pitt.edu
+# date          : 10/25/2016
+# class         : CS0008-f2016
+# instructor    : Max Novelli
+#
+# Description:
+# Assignment 2
+#
+# Ask the user to enter the name of the text file
+file_name = input("Enter the name of the text file: ")
+def processFile(fh):
+    data_1 = open('f2016_cs8_a2.data1.csv', 'r')
+    data_2 = open('f2016_cs8_a2.data2.csv', 'r')
+
+def printKV(key, value, klen=0):
+    data_2 = open('f2016_cs8_a2.data2.csv', 'r')
